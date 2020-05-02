8590de1d4bc0fce4bb8da7ea92d04246bfa88b6b
Игнатов Илья <64230361+e100nets@users.noreply.github.com>
2020-05-02 17:30:35 +0300

    исправлен стиль

--- Triangle.java
+++ Triangle.java
@@ -14,39 +14,39 @@ public class Triangle {
         this.yp = yp;
     }
 
-    public double side_AB() {
+    public double sideAB() {
         return Math.sqrt((Math.pow(x1 - x2, 2)) + ((Math.pow(y1 - y2, 2))));
     }
 
-    public double side_BC() {
+    public double sideBC() {
         return Math.sqrt((Math.pow(x2 - x3, 2)) + ((Math.pow(y2 - y3, 2))));
     }
 
-    public double side_CA() {
+    public double sideCA() {
         return Math.sqrt((Math.pow(x3 - x1, 2)) + ((Math.pow(y3 - y1, 2))));
     }
 
     public double Perimeter() {
-        return side_AB() + side_BC() + side_CA();
+        return sideAB() + sideBC() + sideCA();
     }
 
     public double Square () {
-        double half_meter = Perimeter()/2;
-        return Math.sqrt(half_meter*(half_meter - side_AB())*(half_meter-side_BC())*(half_meter-side_CA()));
+        double halfMeter = Perimeter()/2;
+        return Math.sqrt(halfMeter*(halfMeter - sideAB())*(halfMeter-sideBC())*(halfMeter-sideCA()));
     }
 
     public boolean IsPointInsideTheTriangle () {
-        double side_AP = Math.sqrt((Math.pow(x1-xp, 2))+((Math.pow(y1-yp, 2))));
-        double side_BP = Math.sqrt((Math.pow(x2-xp, 2))+((Math.pow(y2-yp, 2))));
-        double side_CP = Math.sqrt((Math.pow(x3-xp, 2))+((Math.pow(y3-yp, 2))));
+        double sideAP = Math.sqrt((Math.pow(x1-xp, 2))+((Math.pow(y1-yp, 2))));
+        double sideBP = Math.sqrt((Math.pow(x2-xp, 2))+((Math.pow(y2-yp, 2))));
+        double sideCP = Math.sqrt((Math.pow(x3-xp, 2))+((Math.pow(y3-yp, 2))));
 
-        double half_meter1 = (side_AB() + side_AP + side_BP)/2;
-        double half_meter2 = (side_BC() + side_BP + side_CP)/2;
-        double half_meter3 = (side_CA() + side_CP + side_AP)/2;
+        double halfMeter1 = (sideAB() + sideAP + sideBP)/2;
+        double halfMeter2 = (sideBC() + sideBP + sideCP)/2;
+        double halfMeter3 = (sideCA() + sideCP + sideAP)/2;
 
-        double Square1 = Math.sqrt(half_meter1*(half_meter1 - side_AB())*(half_meter1 - side_AP)*(half_meter1 - side_BP));
-        double Square2 = Math.sqrt(half_meter2*(half_meter2 - side_BC())*(half_meter2 - side_BP)*(half_meter2 - side_CP));
-        double Square3 = Math.sqrt(half_meter3*(half_meter3 - side_CA())*(half_meter3 - side_CP)*(half_meter3 - side_AP));
+        double Square1 = Math.sqrt(halfMeter1*(halfMeter1 - sideAB())*(halfMeter1 - sideAP)*(halfMeter1 - sideBP));
+        double Square2 = Math.sqrt(halfMeter2*(halfMeter2 - sideBC())*(halfMeter2 - sideBP)*(halfMeter2 - sideCP));
+        double Square3 = Math.sqrt(halfMeter3*(halfMeter3 - sideCA())*(halfMeter3 - sideCP)*(halfMeter3 - sideAP));
 
         return Square() > (Square1 + Square2 + Square3) * 0.999 && Square() < (Square1 + Square2 + Square3) * 1.001; // задаём погрешность 0,1%
     }
